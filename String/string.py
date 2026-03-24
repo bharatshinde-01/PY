4ec071432e0b9009aff345077aa6529af79b90ae
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-24 21:24:50 +0530

    Add string file

---
+++ String/string.py
@@ -0,0 +1,24 @@
+name = "Bharau" #This is a string
+name1 = 'Bharat'
+name2 = ''' bharat ''' # Hya types ni pan apan string lihu shaktodd
+nameshort = name[0:3]
+print(nameshort) #string & all programing lan is count starts with 0 
+
+#                 #Negative slicing -1,-2
+print(name[-4:-1])
+print(name[1:4])
+print(name[1:])
+print(name[:5]) #is same as print(0:5)
+print(name[-1:-3])
+
+# string slicing with skip value 
+word = "amazing"
+print(word[1 : 6 :2])
+name ="bHarAtsOinDe"
+      #123456789012
+print(name[1:12:2])
+print(name[0:12])
+print(name[1:11:3])
+
+
+
